aef172e35bb8e3c35e9ca97a0de9077c4cb03e9d
Nizar Kamel <nizarekamel@gmail.com>
2022-05-16 18:28:50 -0400

    past 7 day data added

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -8,7 +8,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// We create the dark view tile layer that will be an option for our map.
+// We create the second view tile layer that will be an option for our map.
 let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -23,31 +23,17 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Create a style for the lines.
-let myStyle = {
-  fillColor: "#ffffa1",
-  weight: 1
-}
-
-// Accessing the airport GeoJSON URL
-let torontoHoods = 'https://raw.githubusercontent.com/kamelne/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json'
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+// Retrieve the eathquake GeoJSON data
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data,{
-  style : myStyle,
-  onEachFeature: function(feature,layer){ 
-    layer.bindPopup('<h3> Neighborhood: ' + feature.properties.AREA_NAME + '</h3>')
-  }
-})
-.addTo(map)
+L.geoJSON(data).addTo(map);
 });
